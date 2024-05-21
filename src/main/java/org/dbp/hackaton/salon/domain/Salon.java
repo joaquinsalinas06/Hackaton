7906ae57b49990f0eaeeb9de11f7d06ed5eca78c
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2024-05-21 14:14:05 -0500

    user y salon

---
+++ src/main/java/org/dbp/hackaton/salon/domain/Salon.java
@@ -0,0 +1,20 @@
+package org.dbp.hackaton.salon.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Salon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
+
---
+++ src/main/java/org/dbp/hackaton/usuario/application/UsuarioController.java
@@ -0,0 +1,9 @@
+package org.dbp.hackaton.usuario.application;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+}
